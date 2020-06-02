f397b680c4af40b423a20a70a5e5b089b274b27a
Clinton <clint93@uab.edu>
2020-06-01 21:23:28 -0500

    add project files

---
+++ Handmade_Hero_Project/main.cpp
@@ -0,0 +1,15 @@
+#include <windows.h>
+
+void foo(void)
+{
+    OutputDebugStringA("This is the first thing we have actually printed.\n");
+}
+
+int CALLBACK WinMain(
+        HINSTANCE hInstance,
+        HINSTANCE hPrevInstance,
+        LPSTR lpCmdLine,
+        int nCmdShow)
+{
+    foo();
+}
