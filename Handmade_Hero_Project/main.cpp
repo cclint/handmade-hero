d2ad01870e5e6dced8e0993ef0941c8cc936e8b8
Clinton <clint93@uab.edu>
2020-06-17 18:11:04 -0500

    switch statements for messages

--- Handmade_Hero_Project/main.cpp
+++ Handmade_Hero_Project/main.cpp
@@ -11,25 +11,25 @@ LRESULT CALLBACK MainWindowCallback(
     {
     case WM_SIZE:
         {
-
+            OutputDebugStringA("WM_SIZE\n");
         } break;
 
     case WM_DESTROY:
         {
-
+            OutputDebugStringA("WM_DESTROY\n");
         } break;
     case WM_CLOSE:
         {
-
+            OutputDebugStringA("WM_CLOSE\n");
         } break;
     case WM_ACTIVATEAPP:
         {
-
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
         } break;
 
      default:
         {
-
+    //        OutputDebugStringA("default\n");
         }break;
 
     }
